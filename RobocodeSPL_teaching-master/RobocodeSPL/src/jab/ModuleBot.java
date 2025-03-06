f217892ba531390d6b225435d8477b3cd6b43bf7
sabrinaatha <sabrinaathashania@gmail.com>
2025-03-06 15:48:50 +0700

    completed tugas 2 week 1 #3 edit gitignore

---
+++ RobocodeSPL_teaching-master/RobocodeSPL/src/jab/ModuleBot.java
@@ -0,0 +1,60 @@
+package jab; 
+
+import jab.gun.*; 
+import jab.module.*; 
+import jab.module.Module; 
+import jab.movement.*; 
+import jab.radar.*; 
+import jab.selectEnemy.*; 
+import jab.targeting.*; 
+import java.awt.Color; 
+
+public   class  ModuleBot  extends Module {
+	
+
+	protected void initialize() {
+		// TODO Customize the colors here
+		setBodyColor(Color.BLACK);
+		setGunColor(Color.BLACK);
+		setRadarColor(Color.BLACK);
+		setScanColor(Color.BLUE);
+		setBulletColor(Color.RED);
+	}
+
+	
+
+	protected void selectBehavior() {
+		radar = getSelectedRadar();
+		movement = getSelectedMovement();
+		targeting = getSelectedTargeting();
+		selectEnemy = getSelectedSelectEnemy();
+		gun = getSelectedGun();
+	}
+
+	
+	protected Radar getSelectedRadar() {
+		return new MouseRadar(this);
+	}
+
+	
+	protected Targeting getSelectedTargeting() {		
+		return new HeadOnTargeting(this);
+	}
+
+	
+	protected Movement getSelectedMovement() {		
+		return new Cornering(this);
+	}
+
+	
+	protected SelectEnemy getSelectedSelectEnemy() {		
+		return new Weakest(this);
+	}
+
+	
+	protected Gun getSelectedGun() {		
+		return new CeaseFire(this);
+	}
+
+
+}
---
+++ RobocodeSPL_teaching-master/RobocodeSPL/src/jab/targeting/HeadOnTargeting.java
@@ -0,0 +1,24 @@
+package jab.targeting; 
+
+import jab.module.Module; 
+import jab.module.Targeting; 
+
+public  class  HeadOnTargeting  extends Targeting {
+	
+
+	public HeadOnTargeting(Module bot) {
+		super(bot);
+	}
+
+	
+
+	public void target() {
+		if (bot.enemy != null) {
+			double absoluteBearing = bot.getHeadingRadians() + bot.enemy.bearingRadians;
+			//System.out.println("HT Bearing: " +bot.enemy.bearingRadians + "   AbsoluteBearing: "+absoluteBearing);
+			bot.setTurnGunRightRadians(robocode.util.Utils.normalRelativeAngle(absoluteBearing - bot.getGunHeadingRadians()));
+		}
+	}
+
+
+}
